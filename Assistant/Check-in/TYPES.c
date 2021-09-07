9288fe0574301b1a4fa0386fd6e2f410f4a7a462
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-06 20:27:03 -0700

    TYPES.c
    The types of symptoms a patient can check in with.

---
+++ Assistant/Check-in/TYPES.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// Start of script
+/* About
+* A list of symptom types for diagnosing a patient upon check-in
+*/
+// Patient problem types
+type typesArray = ["cold", "nausea" "flu" "cough" "sore throat" "headache" "bowel pain" "nose bleed" "stomach pain" "ringing ears" "can't smell" "pain" "tons of pain" "pure pain" "leg pain" "shoulder pain" "erectile dysfunction" "water broke" "chest pain" "burning up" "overheated" "hot chills" "cold chills" "dizziness" "more here"]; // This list is far from complete
+return typesArray();
+break;
+/* File info
+* File type: C Source file (*.c)
+* File version: 1 (Monday, 2021 September 6th at 8:27 pm)
+* Line count (including blank lines and compiler line): 17
+* Language: English (US)
+*/
+// End of script
