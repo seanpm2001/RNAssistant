4451890279d4019d4a05b191ebfd010e2eac5720
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-09-06 20:42:01 -0700

    MAIN.c
    The main script for the RN Assistant.

---
+++ Assistant/MAIN.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+// Start of script
+// The main process for the RNA Assistant
+// I am not very good at programming in C. This may appear as bad code and may not compile correctly.
+// Definitions
+union RNAssistant (1);
+// Main
+bname = str("hospital"); // IMPORTANT: Change the portion in parantheseses to the name of the building you are setting this up in (EX: Providence St. Marys Medical Center)
+// I will come up with a way to do this in the program in the future.
+int main(void) {
+    return "///Assistant/check-in/"{ TYPES() }
+    break;
+    exit;
+} // End of main method
+int start();
+{
+  printf("RN Assistant: Welcome to " + str(bname) + " how may I assist you today?");
+  return options();
+  if (options1 == "check-in"); {
+    return TYPES();
+    break;
+  } else if (options1 == "nevermind"); {
+    return endit();
+    break;
+  }
+  break;
+}
+int helloWorld(); // Unused
+{
+  printf("Hello, world!\n");
+  break;
+}
+int settings();
+{
+  changeBName = str(input("What is the name of your medical center? "));
+  scanf(changeBName);
+  confirm1 = bool(input("Confirm name change to " + str(ChangeBName) + " ?"));
+  if (bool == true): {
+    bname == str(changeBName);
+    printf("Your medical center is now titled: " + str(bname));
+    break;
+  } else if (bool == false): {
+    printf("Hospital name remains the same, set to " + str(bname));
+    break;
+  }
+  // Next step
+  printf("Hospital settings setup complete. Enjoy using RNAssistant!");
+  break;
+  // There are no more steps at the moment
+}
+/* File info
+* File version: `1 (Monday, 2021 September 6th at 8:42 pm)
+* File type: C Source file (*.c)
+* Line count (including blank lines and compiler line): 59
+*/
+// This script is incomplete, and needs lots of work
+// End of script
